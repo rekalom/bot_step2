549a46147badc00bf6fe456771614a0a95abafa1
Vasiliy Malashenko <valavas@mail.ru>
2023-08-23 15:32:52 +0300

    Модульный эхобот

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
 
 # Функция конфигурирования и запуска бота
 async def main() -> None:
@@ -13,11 +14,15 @@ async def main() -> None:
     bot: Bot = Bot(token=config.tg_bot.token)
     dp: Dispatcher = Dispatcher()
 
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     asyncio.run(main())
 
 # Выводим значения полей экземпляра класса Config на печать,
